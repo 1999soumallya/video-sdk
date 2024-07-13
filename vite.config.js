a12d7c8f8d33c209fbacf25ab71e1cdc29271d92
Soumallya Dey <89212767+1999soumallya@users.noreply.github.com>
2024-07-13 10:39:55 +0530

    Complete project setup in vite

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,32 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, 'node_modules', '@zoom', 'videosdk', 'dist', 'lib'),
+          dest: 'public/lib',
+        },
+      ],
+    }),
+  ],
   server: {
     headers: {
-      'Cross-Origin-Embedded-Policy': 'require-crop',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
     },
-    watch: {
-      usePolling: true,
-    },
-    fs: {
-      strict: false
-    }
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+  preview: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
   }
 })
